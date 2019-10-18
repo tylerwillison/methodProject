09136284520910b6ca6bf9b389f6ccd760bd541b
Tyler <tyler.willison.2022@student.lhschools.org>
2019-10-18 09:46:01 -0400

    Setup Scanner

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,9 +1,15 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
     System.out.println("Hello World");
+    Scanner keyboard;
+    keyboard = new Scanner (System.in);
+
+
 
 
     }
